2a5247a14b39aed58c3578d4e4c33e8f2f223596
Chris Moran <chris.moran@emergent.com.au>
2016-07-11 09:57:20 +1000

    Adding Team Framework

---
+++ src/test/java/com/perrinn/appservice/team/TeamTest.java
@@ -0,0 +1,40 @@
+package com.perrinn.appservice.team;
+
+import java.util.Random;
+import org.junit.Test;
+import org.junit.Assert;
+
+public class TeamTest {
+	@Test
+	public void testId() {
+		Team t = new Team();
+		Random r = new Random();
+		int p = 0;
+		while (p < 1000) {
+			long l = r.nextLong();
+			t.setId(l);
+			Assert.assertEquals(l, t.getId());
+			p++;
+		}
+	}
+
+	@Test
+	public void testTeamOwner() {
+		Team t = new Team();
+		Random r = new Random();
+		int p = 0;
+		while (p < 1000) {
+			long l = r.nextLong();
+			t.setTeamOwner(l);
+			Assert.assertEquals(l, t.getTeamOwner());
+			p++;
+		}
+	}
+
+	@Test
+	public void testTeamName() {
+		Team t = new Team();
+		t.setTeamName("Team");
+		Assert.assertEquals("Team", t.getTeamName());
+	}
+}
