cd3f71f7eb90888dc5d4a0b1d83c0d2ed8622b34
Chris Moran <chris.moran@emergent.com.au>
2016-08-27 21:02:33 +1000

    Adding workable config.properties for automated testing

--- src/main/java/com/perrinn/appservice/team/Team.java
+++ src/main/java/com/perrinn/appservice/team/Team.java
@@ -6,6 +6,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.sql.*;
+import com.perrinn.appservice.util.Config;
 
 @Entity
 @Table(name="team")
@@ -17,6 +19,8 @@ public class Team {
 
 	private long teamOwner;
 	private String teamName;
+
+	private Connection conn;
 
 	// Getters
 	public long getId() {
@@ -48,6 +52,7 @@ public class Team {
 		this.id = 0;
 		this.teamOwner = 0;
 		this.teamName = null;
+		this.conn = null;
 	}
 
 	public Team() {
@@ -62,5 +67,120 @@ public class Team {
 	public Team(String name) {
 		this.initLocals();
 		this.teamName = name;
+	}
+
+	private boolean open() {
+		boolean ret = false;
+
+		Config conf = new Config();
+		try {
+			Class.forName(conf.getDatabaseDriver());
+			this.conn = DriverManager.getConnection(conf.getDatabaseString(), conf.getDatabaseUser(), conf.getDatabasePassword());
+		}
+		catch(Exception ex) {
+			System.err.println(ex.getMessage());
+			ret = true;
+		}
+
+		return ret;
+	}
+	public boolean close() {
+    	boolean ret = false;
+    	
+    	try {
+    		if(this.conn.isClosed() == false) {
+    			this.conn.close();
+    		}
+    	}
+    	catch(Exception ex) {
+    		System.err.println(ex.toString());
+    	}
+    	
+    	return ret;
+	}
+
+	public long loadById() {
+		PreparedStatement stmt = null;
+		String sql = null;
+		ResultSet rs = null;
+
+		if(this.open() == false) {
+			// Connection is initialised.
+			try {
+				sql = "select * from team where id= ?";
+				stmt = this.conn.prepareStatement(sql);
+				stmt.setLong(1, this.id);
+				rs = stmt.executeQuery(sql);
+				while(rs.next()) {
+					this.teamOwner = rs.getLong("team_owner");
+					this.teamName = rs.getString("team_name");
+				}
+			}
+			catch(Exception ex) {
+				System.err.println(ex.toString());
+				this.id = 0; // The entry is not available.
+			} 
+			finally {
+				try {
+					if(rs != null) {
+						rs.close();
+					}
+					if(stmt != null) {
+						stmt.close();
+					}
+					this.close();
+				}
+				catch(Exception ex) {
+					System.err.println(ex.toString());
+				}
+			}
+		}
+		else {
+			this.id = 0;	// Database not open.  Didn't get anything
+		}
+
+		return this.id;
+	}
+
+	public boolean save() {
+		boolean ret = false;
+		PreparedStatement stmt = null;
+		String sql = null;
+		ResultSet rs = null;
+
+		if(this.open() == false) {
+			// Connection is initialised.
+			try {
+				sql = "update team set team_name=?, team_owner=? where id= ?";
+				stmt = this.conn.prepareStatement(sql);
+				stmt.setString(1, this.teamName);
+				stmt.setLong(2, this.teamOwner);
+				stmt.setLong(3, this.id);
+			}
+			catch(Exception ex) {
+				System.err.println(ex.toString());
+				this.id = 0; // The entry is not available.
+			} 
+			finally {
+				try {
+					if(rs != null) {
+						rs.close();
+					}
+					if(stmt != null) {
+						stmt.close();
+					}
+					this.close();
+				}
+				catch(Exception ex) {
+					System.err.println(ex.toString());
+					ret = true;
+				}
+			}
+		}
+		else {
+			ret = true;	// Database not open.  Didn't get anything
+		}
+
+		return ret;
 	}
 }
